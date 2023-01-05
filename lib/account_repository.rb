3a9bbb3c4f973bf6d00d709c2f0b44a7e8e63dbf
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 13:47:09 +0000

    Implemented #all method, passes test

--- lib/account_repository.rb
+++ lib/account_repository.rb
@@ -1,7 +1,14 @@
 class AccountRepository
 
   def all
-
+    query = "SELECT * FROM accounts"
+    records = DatabaseConnection.exec_params(query, [])
+    arr = records.map do |record|
+      Account.new(record["username"],
+      record["email_address"], 
+      record["id"].to_i)
+    end
+    arr
   end
 
   def find
