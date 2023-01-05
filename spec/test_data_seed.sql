f5f3b09b83ef37780f2cb05dff0c0be0b4456df8
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 14:44:47 +0000

    Fixed syntax error, now works correctly

--- spec/test_data_seed.sql
+++ spec/test_data_seed.sql
@@ -4,7 +4,7 @@ INSERT INTO accounts (username, email_address) VALUES ('tmccoy', 'tmccoy@gmail.c
 INSERT INTO accounts (username, email_address) VALUES ('great_account_name', 'great_email@talktalk.net');
 
 INSERT INTO posts (title, content, view_count, account_id)
-VALUES ("Summer 2022", "OMG glasto was amazing", 2, 1)
+VALUES ('Summer 2022', 'OMG glasto was amazing', 2, 1);
 
 INSERT INTO posts (title, content, view_count, account_id)
-VALUES ("Spring 2022", "Exams are like so stressful", 10, 1)
+VALUES ('Spring 2022', 'Exams are like so stressful', 10, 1);
