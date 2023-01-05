1ee3c26db8ad0e1dd880f7897db00f03c9d42600
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 14:41:12 +0000

    Added PostRepository test data to file and renamed accordingly

--- spec/test_data_seed.sql
+++ spec/test_data_seed.sql
@@ -2,3 +2,9 @@ TRUNCATE TABLE posts, accounts RESTART IDENTITY;
 
 INSERT INTO accounts (username, email_address) VALUES ('tmccoy', 'tmccoy@gmail.com');
 INSERT INTO accounts (username, email_address) VALUES ('great_account_name', 'great_email@talktalk.net');
+
+INSERT INTO posts (title, content, view_count, account_id)
+VALUES ("Summer 2022", "OMG glasto was amazing", 2, 1)
+
+INSERT INTO posts (title, content, view_count, account_id)
+VALUES ("Spring 2022", "Exams are like so stressful", 10, 1)
