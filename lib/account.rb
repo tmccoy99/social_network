733abdc76055bc34895c019d55600cd524b0f864
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 13:32:53 +0000

    Defined instance variables attr_accessor which can be manually set or passed in with initialise

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,9 @@
 class Account
 
+  attr_accessor :id, :username, :email_address
+
+  def initialize(username = nil, email_address = nil, id = nil)
+    @username = username ; @email_address = email_address ; @id = id
+  end
+  
 end
